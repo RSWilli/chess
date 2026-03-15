ef961c2a4352edb921789d080d45842ec5489765
RSWilli <bartel.wilhelm@gmail.com>
2026-03-15 17:43:41 +0100

    allow to configure the address of the UI server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"net/http"
 
 	"github.com/rswilli/chess/internal/api"
 )
 
+var addr = flag.String("addr", ":3000", "listen address of the server")
+
 func main() {
+	flag.Parse()
+
 	h := api.NewHandler()
 
 	serv := http.Server{
-		Addr:    ":3000",
+		Addr:    *addr,
 		Handler: h,
 	}
 
-	slog.Info("starting server", "addr", "localhost:3000")
+	slog.Info("starting server", "addr", *addr)
 	err := serv.ListenAndServe()
 
 	slog.Error("server ended", "error", err)
